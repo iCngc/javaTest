b94990d99f26c6f672bf08c1eefceebb0496d9f4
admin <icngc@outlook.com>
2018-10-30 20:43:28 +0800

    this is test

---
+++ src/main/java/com/icngc/test/Test.java
@@ -0,0 +1,11 @@
+package com.icngc.test;
+
+/**
+ * @author: Mr.Wang
+ * @create: 2018-10-30 20:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hell word");
+    }
+}
